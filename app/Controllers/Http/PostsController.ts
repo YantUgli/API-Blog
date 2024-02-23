29f2e628ebcde931baf7ebbaff24caf428cbe602
Hillindo <bryantnanur@gmail.com>
2024-02-23 23:16:57 +0700

    slug generator and validation

--- app/Controllers/Http/PostsController.ts
+++ app/Controllers/Http/PostsController.ts
@@ -1,6 +1,6 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Post from 'App/Models/Post'
-import { schema } from '@ioc:Adonis/Core/Validator'
+import { schema, rules } from '@ioc:Adonis/Core/Validator'
 
 export default class PostsController {
   async index() {
@@ -9,16 +9,35 @@ export default class PostsController {
   }
 
   async store({ request }: HttpContextContract) {
-
     const newPostData = schema.create({
       title: schema.string(),
-      categoryId: schema.number.optional()
+      categoryId: schema.number.optional(),
+      slug: schema.string.optional([rules.unique({ table: 'posts', column: 'slug' })]),
     })
-    const data = await request.validate({schema: newPostData})
+    const dataBody = request.body()
+    const slug = this.generateSlug(dataBody.title)
+    dataBody.slug = slug
+    console.log(request)
 
+    const data = await request.validate({
+      schema: newPostData,
+      data: dataBody,
+      messages: {
+        required: 'The {{ field }} is {{rule}} to create a post ',
+      },
+    })
 
     const post = await Post.create(data)
 
     return post
+  }
+
+  // Fungsi untuk mengubah title menjadi slug
+  private generateSlug(title: string): string {
+    const slug = title
+      .toLowerCase()
+      .replace(/\s+/g, '-') // Mengganti spasi dengan tanda strip
+      .replace(/[^\w\-]+/g, '') // Menghapus karakter non-word dan non-dash
+    return slug
   }
 }
